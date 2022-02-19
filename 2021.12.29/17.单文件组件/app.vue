beb0b7dfdb6c2eb850c0c66a1a22b114e70c775b
yuconcat <1549021206@qq.com>
2022-02-19 22:01:00 +0800

    Initial commit

---
+++ 2021.12.29/17.单文件组件/app.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <school></school>
+        <Student></Student>
+    </div>
+</template>
+
+<script>
+    //引入组件
+    import School from "./School.vue"
+    import Student from "./Student.vue"
+    export default {
+        name: "app",
+        components:{
+            School:School,
+            Student:Student
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ 2021.12.29/17.单文件组件/main.js
@@ -0,0 +1,7 @@
+import app from "./app.vue"
+
+new Vue({
+    el:"#root",
+    components:{app},
+    template:`<app></app>`
+})
